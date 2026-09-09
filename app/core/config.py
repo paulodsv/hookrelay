645b041d415c0d832f205d9887ecb44b992125bc
paulodsv <paulovieiradias064@gmail.com>
2026-09-09 14:46:08 -0300

    feat: add JWT env vars

--- app/core/config.py
+++ app/core/config.py
@@ -10,6 +10,10 @@ class Settings(BaseSettings):
     RABBITMQ_USER: str
     RABBITMQ_PASSWORD: str
 
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str 
+    JWT_EXPIRE_MINUTES: int 
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
