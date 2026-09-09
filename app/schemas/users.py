3ba4911931376ff4ccbec40a26768b2787f266db
paulodsv <paulovieiradias064@gmail.com>
2026-09-09 14:48:52 -0300

    feat: add UserLogin and TokenResponse schemas

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -4,4 +4,11 @@ class CreateUser(BaseModel):
     name: str
     email: EmailStr
     password: str
-    
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
