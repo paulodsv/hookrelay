05b9eee1fd7f7d175b3c2eea2a50cba82c17f42b
paulodsv <paulovieiradias064@gmail.com>
2026-09-09 14:47:07 -0300

    feat: add JWT Access Token Creation

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,7 @@
 from pwdlib import PasswordHash
+import jwt
+from app.core.config import settings
+from datetime import datetime, timedelta, timezone
 
 
 password_hash = PasswordHash.recommended()
@@ -10,3 +13,15 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hashed_password: str) -> bool:
     return password_hash.verify(password, hashed_password)
+
+def create_access_token(user_id: str) -> str:
+    expires_at = datetime.now(timezone.utc) + timedelta (minutes=settings.JWT_EXPIRE_MINUTES)
+
+    payload = {
+        "sub": str(user_id),
+        "exp": expires_at,
+    }
+
+    return jwt.encode(payload, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM,)
+    
+
