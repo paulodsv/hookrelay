86633532d2c92afd8d851086f865711f6bd47e65
Paulinho <paulovieiradias064@gmail.com>
2026-09-10 15:48:30 -0300

    refactor: replace oauth with httpbearer

--- app/core/deps.py
+++ app/core/deps.py
@@ -1,8 +1,10 @@
 from fastapi import Request, Depends
 from app.repository.users import UserRepository
+from app.repository.destinations import DestinationsRepository
 from app.service.users import UserService
+from app.service.destinations import DestinationsService
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from app.core.security import decode_access_token
 
 async def get_db(request: Request):
@@ -11,12 +13,23 @@ async def get_db(request: Request):
 
 async def get_user_service(db = Depends(get_db)):
     user_repo = UserRepository(db)
-    service = UserService(db, user_repo)
+    service = UserService(user_repo)
     return service
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+async def get_destination_service(db = Depends(get_db)):
+    destination_repo = DestinationsRepository(db)
+    service = DestinationsService(destination_repo)
+    return service
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db = Depends(get_db)):
+
+
+bearer_scheme = HTTPBearer()
+
+async def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme), 
+    db = Depends(get_db)
+):
+    token = credentials.credentials
     payload = decode_access_token(token)
 
     user_id = payload.get("sub")
